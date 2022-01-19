7645f1c1bf60899494249b0d0cf6cb4b41077ee5
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-01-19 23:58:22 +0900

    test
    test to join

--- src/main/java/com/koreait/funfume/controller/shop/LoginController.java
+++ src/main/java/com/koreait/funfume/controller/shop/LoginController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.koreait.funfume.domain.Member;
 import com.koreait.funfume.exception.MemberException;
@@ -26,108 +22,107 @@ import com.koreait.funfume.util.Message;
 
 @Controller
 public class LoginController {
-	
-	
+
 	@Autowired
 	MemberService memberService;
+
 	@Autowired
 	private HashBuilder hashBuilder;
-	
-	
-	//로그인 폼 요청
-		@GetMapping("/sign-in-form")
-		public String signupForm() throws Exception {
-			
-			return "shop/sign-in";
-		}
-	
-	//로그인처리
+
+	// 로그인 폼 요청
+	@GetMapping("/sign-in-form")
+	public String signupForm() throws Exception {
+
+		return "shop/sign-in";
+	}
+
+	// 로그인처리
 	@PostMapping("/signIn")
-	@ResponseBody  //return 값에 반환된 데이터를 viewResolver가 해석하는게 아니라 메서드의 반환형으로 명시한 데이터 자체를 응답데이터로 전송
-	public Message signIn(HttpServletRequest request, Member member){
-		
-		//서비스에 일 시키기 전에 , 비밀번호 hash값으로 변환해서 비교.
-		//db의 hash값과 , 변환한 hash값이 틀린경우 인증실패.
-		
+	@ResponseBody // return 값에 반환된 데이터를 viewResolver가 해석하는게 아니라 메서드의 반환형으로 명시한 데이터 자체를 응답데이터로 전송
+	public Message signIn(HttpServletRequest request, Member member) {
+
+		// 서비스에 일 시키기 전에 , 비밀번호 hash값으로 변환해서 비교.
+		// db의 hash값과 , 변환한 hash값이 틀린경우 인증실패.
+
 		String pass = hashBuilder.convertStringToHash(member.getPass());
 		member.setPass(pass);
-		
+
 		Member result = memberService.signIn(member);
-		
+
 		Message message = new Message();
 		message.setCode(1);
 		message.setMsg("로그인되었습니다.");
-		
-		//클라이언트가 재접속시 데이터를 사용할 수 있도록 session에 result담자.
+
+		// 클라이언트가 재접속시 데이터를 사용할 수 있도록 session에 result담자.
 		HttpSession session = request.getSession();
-		session.setAttribute("member", result); //세션에 DTO저장
+		session.setAttribute("member", result); // 세션에 DTO저장
 		return message;
 	}
-	
-	
-	//로그아웃 요청 처리
+
+	// 로그아웃 요청 처리
 	@GetMapping("/logout")
 	public String logout(HttpServletRequest request) {
 		HttpSession session = request.getSession();
-		session.invalidate();//세션을 무효화... 기존의 세션을 사용할 수 없게 됨
-		
+		session.invalidate();// 세션을 무효화... 기존의 세션을 사용할 수 없게 됨
+
 		return "redirect:/sign-in-form";
 	}
-	
-	
-	@ExceptionHandler(MemberException.class) 
+
+	@ExceptionHandler(MemberException.class)
 	@ResponseBody
 	public ResponseEntity<Message> handle(MemberException e) {
 		HttpHeaders header = new HttpHeaders();
 		header.add("Content-Type", "text/html;charset=utf-8");
-	
-		//한글 및 제대로된 응답정보를 구성하려면, ResponseEntity header,body { code:1,msg:"실패입니다" }  <-이런 구조화된 형태를 응답해주자
-		//Gson을 직접써도되지만, 로드존슨이 이미 자동으로 json으로 변환하는 내부적 처리를 했다.   객체 -->JSON
+
+		// 한글 및 제대로된 응답정보를 구성하려면, ResponseEntity header,body { code:1,msg:"실패입니다" } <-이런
+		// 구조화된 형태를 응답해주자
+		// Gson을 직접써도되지만, 로드존슨이 이미 자동으로 json으로 변환하는 내부적 처리를 했다. 객체 -->JSON
 		Message message = new Message();
 		message.setCode(0);
 		message.setMsg(e.getMessage());
-		
-		ResponseEntity <Message> entity=new ResponseEntity(message, HttpStatus.OK);
+
+		ResponseEntity<Message> entity = new ResponseEntity(message, HttpStatus.OK);
 		return entity;
 	}
-		
-	
-	//회원가입 폼 요청
-	@GetMapping("/memberForm")
-	public String MemberForm(HttpServletRequest request){
-		
+
+	// 회원가입 폼 요청
+	@GetMapping("/joinForm")
+	public String MemberForm() {
 		return "shop/join";
 	}
-	
-	//회원가입시 이메일 중복체크
+
+	@PostMapping("/join")
+	public String join(HttpServletRequest request, Member member) {
+		System.out.println(member.getAddr1());
+		memberService.join(member);
+		return "redirect:/sign-in-form";
+	}
+
+	// 가입회원 비밀번호 암호화
+	/*
+	 * @PostMapping("/join") public String join(Member member, HttpServletRequest
+	 * request) throws Exception{ request.setCharacterEncoding("utf-8");
+	 * request.getParameter("utf-8"); System.out.println(member.getAddr1());
+	 * System.out.println(member.getAddr2()); String
+	 * pass=hashBuilder.convertStringToHash(member.getPass()); member.setPass(pass);
+	 * 
+	 * 
+	 * memberService.join(member);
+	 * 
+	 * return "redirect:/sign-in-form";
+	 * 
+	 * }
+	 */
+	// 회원가입시 이메일 중복체크
 	@PostMapping("/emailCheck")
 	@ResponseBody
 	public int emailCheck(@RequestParam("email") String email) {
 		System.out.println("emailCheck진입");
 		System.out.println("전달받은email : " + email);
 		int result = memberService.emailCheck(email);
-		System.out.println("확인결과 : "+ result);
-		
+		System.out.println("확인결과 : " + result);
+
 		return result;
 	}
-	
-	
-	// 가입회원 비밀번호 암호화
-	@RequestMapping(value="/join" ,method = RequestMethod.POST)
-	public String join(Member member) throws Exception{
-		HttpHeaders header = new HttpHeaders();
-		header.add("Content-Type", "text/html;charset=utf-8");
-	
-		System.out.println(member.getAddr1());          
-		System.out.println(member.getAddr2());          
-		String pass=hashBuilder.convertStringToHash(member.getPass());
-		member.setPass(pass);
-		
-		
-		memberService.join(member);
 
-		return "redirect:/sign-in-form";
-		
-	}
-	
 }
--- src/main/webapp/WEB-INF/views/shop/join.jsp
+++ src/main/webapp/WEB-INF/views/shop/join.jsp
@@ -1,44 +1,113 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"  pageEncoding="UTF-8"%>
-
+<%@ page contentType="text/html;charset=UTF-8"%>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Insert title here</title>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<!--  Head Link -->
+<%@ include file="../shop_inc/head_link.jsp"%>	
 </head>
-<body>
-
-
-회원가입 폼
-<form name="form1">
-	<input type="text" name="id" placeholder="ID">
-	<input type="email" name="email" placeholder="email">
-	<input type="password" name="pass" placeholder="password">
-	<input type="text" name="addr1" placeholder="addr1">
-	<input type="text" name="addr2" placeholder="addr2">
-	<button type="button" id="regist">가입하기</button>
+<body class="animsition">
 	
-</form>
+	<!-- Header -->
+<%@ include file="../shop_inc/header.jsp" %>
+	<!-- Cart -->
+<%@ include file="../shop_inc/cart.jsp" %>
 
+     <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+       	
+        
+        <!-- Note Regist Form  begin-->
+          <div class="col-9">
+            <div class="card card-warning">
+              <div class="card-header">
+                <h3 class="card-title">INPUT</h3>
+              </div>
+              <!-- /.card-header -->
+              <form name ="form1">
+                <div class="card-body">
+                  <div class="form-group">
+                    <input type="text" class="form-control"  placeholder="이름을 입력하세요" name="member_name">
+                  </div>
+                  <div class="form-group">
+                    <input type="email" class="form-control"  placeholder="id는 email입니다" name="email">
+                  </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control" id="pass1" placeholder="비밀번호를 입력하세요" name="pass">
+                  </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control" id="pass2"  placeholder="비밀번호를 한번더 입력하세요" name="passCheck">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="주소를 입력하세요" name="addr1">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="상세주소를 입력하세요" name="addr2">
+                  </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <button type="button" class="btn btn-warning" onClick="regist()">등록</button>
+                  <button type="button" class="btn btn-warning" onClick="history.back()">취소</button>
+                </div>
+              </form>
+            </div>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+        
+        <!-- /.row (main row) -->
+	    </section>
+      </div>
+      <!-- /.container-fluid -->
+    <!-- /.content -->
+  
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+<!-- ./wrapper -->
+
+
+	<!-- Footer -->
+<%@ include file="../shop_inc/footer.jsp" %>
+	<!-- Back to top -->
+<%@ include file="../shop_inc/back_to_top.jsp" %>
+	<!-- Bottom_link -->
+<%@ include file="../shop_inc/bottom_link.jsp" %>
 
 <script>
-$(function () {
-	$("#regist").click(function(){
-		join();
-	});
+function regist(){
+	//유효성 체크 
+	if($("input[name='member_name']").val()==""){
+		alert("이름은 필수입니다");
+		return;
+	}
+	if($("input[name='email']").val()==""){
+		alert("이메일 입력은 필수입니다");
+		return;	
+	}
+	if($("input[name='pass']").val()==""){
+		alert("비밀번호 입력은 필수입니다");
+		return;
+	}
+	if($("input[name='pass']").val()!=$("input[name='passCheck']").val()){
+		alert("비밀번호가 일치하지 않습니다. 다시확인해주세요");
+	}
 	
-});
-
-
-function join(){
-	form1.action="/join";
-	form1.method="post";
-	form1.submit();
-}
-
+	 $("form[name='form1']").attr({
+		action: "/join",
+		method: "post"
+	 });
+	 $("form[name='form1']").submit();
+  }
 </script>
-
 </body>
 </html>
